027d55536b11c126fd50b977bf22104865ebdd3a
Deepak <www.deepakkrishna9845@gmail.com>
2025-08-05 23:55:30 +0530

    Segregated code files into algorithms and data structure basics

--- Arrays/Basics/arrays.py
+++ Arrays/Basics/arrays.py
@@ -1,10 +1,11 @@
 import array
+import numpy as np
 
+# create arrays using the in-built array module
 my_array = array.array('i', [1,2,3,4,5,6,7,8,9,0])
 print(my_array)
 
-import numpy as np
-
+# create arrays using numpy
 np_array = np.array([1,2,3,4,5,6,7,8,9,0], dtype=int)
 print(np_array)
 
